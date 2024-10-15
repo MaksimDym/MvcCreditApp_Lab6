4a2fa48d14a428d0098002b54c0850eb81b52af8
Maksim <fanmaxon@yandex.ru>
2024-10-15 22:49:35 +0300

    Добавьте файлы проекта.

---
+++ MvcCreditApp/Models/CreditContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCreditApp.Models;
+using MvcCreditApp.Data;
+
+
+namespace MvcCreditApp.Data
+{
+    public class CreditContext : DbContext
+    {
+        public CreditContext(DbContextOptions<CreditContext> options)
+   : base(options)
+        { }
+        public DbSet<Credit> Credits { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+
+
+    }
+}
